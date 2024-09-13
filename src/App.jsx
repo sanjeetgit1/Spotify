04632a8ae638d3359414edbc3dc377a70ca30cc7
sanjeetgit1 <sksanjeet616@gmail.com>
2024-09-14 01:47:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+
+const App = () => {
+  return (
+
+    <div className=' h-screen bg-black text-white'> 
+    <div className=' h-[90%] flex'>
+<Sidebar/>
+  <Display/>
+    </div>
+     <Player/>
+   
+    </div>
+  )
+}
+
+export default App
