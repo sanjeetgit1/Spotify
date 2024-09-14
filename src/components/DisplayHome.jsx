3077f0157c2a043fed404c6bca30ecb6eb63ca4f
sanjeetgit1 <sksanjeet616@gmail.com>
2024-09-15 01:00:38 +0530

    create box

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import Navbar from './Navbar.jsx'
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import { albumsData, songsData } from "../assets/assets.js";
+import AlbumItem from "./AlbumItem.jsx";
+import SongItem from "./SongItem.jsx";
 
 const DisplayHome = () => {
   return (
     <>
-      <Navbar/>
-    </>
-  )
-}
+      <Navbar />
+      <div className=" mb-4 ">
+        <h1 className=" my-5 font-bold text-2xl">Featured Charts</h1>
 
-export default DisplayHome
+        <div className=" flex overflow-auto ">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className=" mb-4 ">
+        <h1 className=" my-5 font-bold text-2xl">Featured Charts</h1>
+
+        <div className=" flex overflow-auto ">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
